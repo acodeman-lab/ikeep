eb9f7f66bdfe19eb6c4c6537790999252a1fbb2a
vignesh15 <vignesh@waitwhile.com>
2021-03-11 03:43:35 +0530

    save users data

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const userRoute = express.Router();
+const  users = require('../models/user.model');
 
-userRoute.get('/',(req,res) =>{
-   res.send('home user');
+userRoute.post('/login',(req,res) =>{
+   res.send('login api');
 });
+
+userRoute.post('/sign-up', async (req, res)=>{
+   const signupPayload = new users(req.body);
+   try {
+      const saveUsers = await signupPayload.save();
+      res.status(201).json(saveUsers);
+   } catch (error) {
+      res.status(400).json({message: error});
+   }
+})
 
 module.exports = userRoute;
